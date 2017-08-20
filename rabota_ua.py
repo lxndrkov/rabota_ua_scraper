7c70919b870b72ce39fed3faca00b494cf88972a
Alexandre <ipatrool@gmail.com>
2017-08-21 00:19:13 +0300

    PEP8

--- rabota_ua.py
+++ rabota_ua.py
@@ -2,51 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-headers = {'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
-}
 
-def get_html(url, headers=headers):
+def get_html(url):
     r = requests.get(url)
     return r.text
 
 
 def get_total_pages(html):
     soup_scraper = BeautifulSoup(html, 'lxml')
-    pages = soup_scraper.find('dl', class_ = 'f-pagination').find_all('a')[-2].get('href')
+    pages = soup_scraper.find('dl', class_='f-pagination').find_all('a')[-2].get('href')
     total_pages = pages.split('=')[2]
     return int(total_pages)
+
 
 def write_csv(data):
     with open('rabota_ua.csv', 'a+') as b:
         writer = csv.writer(b)
-        writer.writerow( (data['title'],
-                        data['salary'],
-                        data['company_name']) )
+        writer.writerow((data['title'],
+                         data['salary'],
+                         data['company_name']))
+
 
 def get_page_data(html):
     soup_scraper = BeautifulSoup(html, 'lxml')
-    vacancies_list = soup_scraper.find('table', class_ = 'f-vacancylist-tablewrap')
+    vacancies_list = soup_scraper.find('table', class_='f-vacancylist-tablewrap')
 
     for vacancy in vacancies_list:
         try:
-            title = vacancy.find('h3', class_ = 'fd-beefy-gunso f-vacancylist-vacancytitle').text.strip()
-            # print('Вакансия:', title, '|', end=" ")
+            title = vacancy.find('h3', class_='fd-beefy-gunso f-vacancylist-vacancytitle').text.strip()
         except:
             title = ''
         try:
-            salary = vacancy.find('p', class_ = 'fd-beefy-soldier -price').text.replace(u'\xa0', u' ')
-            # print('Зароботная плата:', salary, '|', end=" ")
+            salary = vacancy.find('p', class_='fd-beefy-soldier -price').text.replace(u'\xa0', u' ')
         except:
             salary = ''
         try:
-            company_name = vacancy.find('p', class_ = 'f-vacancylist-companyname').text.strip()
-            # print('Работодатель:', company_name)
+            company_name = vacancy.find('p', class_='f-vacancylist-companyname').text.strip()
         except:
             company_name = ''
 
         data = {'title': title,
-        'salary': salary,
-        'company_name': company_name,}
+                'salary': salary,
+                'company_name': company_name, }
 
         write_csv(data)
 
